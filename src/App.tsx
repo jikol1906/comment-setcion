59fc09a46bf3c76283f0ea5a5d38f74a14a86e14
Boris Grunwald <borisgrunwald@gmail.com>
2022-02-17 20:10:48 +0100

    created commentreplythread container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { Box, Grid } from "theme-ui";
+import { Box, Flex, Grid } from "theme-ui";
 import Comment from "./Components/Comment/Comment";
+import CommentReplyThread from "./Layout/CommentReplyThread/CommentReplyThread";
 /** @jsxImportSource theme-ui */
 
 function App() {
   return (
     <Grid p="1" gap="1">
-      <Comment/>
-      <Comment/>
+      <Comment />
+      <Comment />
+      <CommentReplyThread>
+        <Comment />
+        <Comment />
+      </CommentReplyThread>
+
     </Grid>
   );
 }
---
+++ src/Layout/CommentReplyThread/CommentReplyThread.tsx
@@ -0,0 +1,29 @@
+/** @jsxImportSource theme-ui */
+import { Flex } from "theme-ui";
+
+interface ICommentReplyThreadProps {}
+
+const CommentReplyThread: React.FunctionComponent<ICommentReplyThreadProps> = (
+  {children}
+) => {
+  return (
+    <Flex
+      sx={{
+        "&::before": {
+          content: "''",
+          display: "block",
+          width: "3px",
+          bg: "lightgrayishblue",
+          marginRight: "1",
+          flexShrink: 0,
+        },
+      }}
+    >
+      <Flex sx={{ flexDirection: "column", gap: 1 }}>
+        {children}
+      </Flex>
+    </Flex>
+  );
+};
+
+export default CommentReplyThread;
