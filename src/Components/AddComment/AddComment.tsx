da88a912f9066c75260bd7fcd99d4cdcca02dc0b
Boris Grunwald <borisgrunwald@gmail.com>
2022-02-18 12:57:42 +0100

    wip

--- src/Components/AddComment/AddComment.tsx
+++ src/Components/AddComment/AddComment.tsx
@@ -1,20 +1,32 @@
-import * as React from 'react';
-import { Avatar, Grid, Textarea } from 'theme-ui';
-import CommentContainer from '../../Layout/CommentContainer/CommentContainer';
+import * as React from "react";
+import { Avatar, Box, Button, Grid, Textarea } from "theme-ui";
+import CommentContainer from "../../Layout/CommentContainer/CommentContainer";
 import img from "../../images/avatars/image-amyrobson.png";
 
-interface IAddCommentProps {
-}
+interface IAddCommentProps {}
 
 const AddComment: React.FunctionComponent<IAddCommentProps> = (props) => {
-  return <CommentContainer>
-      <Grid gap={1} sx={{
-          gridTemplateColumns:"auto 1fr auto"
-      }}>
-          <Avatar src={img}/>
-          <Textarea sx={{resize:'none',fontSize:"2rem"}} rows={10}></Textarea>
+  return (
+    <CommentContainer>
+      <Grid
+        gap={1}
+        sx={{
+          gridTemplateAreas: `
+          "textarea textarea textarea"
+          "avatar   .       sendbtn"
+          `,
+          gridTemplateColumns: "auto 1fr auto",
+          alignItems: "flex-start",
+        }}
+      >
+        <Avatar style={{ gridArea: "avatar" }} src={img} />
+        <Box as="form" id="sendcommentform" style={{ gridArea: "textarea" }}>
+          <Textarea rows={6}></Textarea>
+        </Box>
+        <Button type="submit" form="sendcommentform" style={{ gridArea: "sendbtn" }}>Send</Button>
       </Grid>
-  </CommentContainer>;
+    </CommentContainer>
+  );
 };
 
 export default AddComment;
