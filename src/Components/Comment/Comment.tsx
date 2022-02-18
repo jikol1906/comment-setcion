94600a4fa4708b978dbb04e9733d55e7158a9800
Boris Grunwald <borisgrunwald@gmail.com>
2022-02-18 12:31:48 +0100

    button container flex

--- src/Components/Comment/Comment.tsx
+++ src/Components/Comment/Comment.tsx
@@ -65,7 +65,7 @@ const PresentCommentText : React.FunctionComponent<IPresentCommentText> = ({text
           <Text as="p" variant="muted" sx={{ gridArea: "content" }}>
           {text}
         </Text>
-      <Box sx={{gridArea:'actions'}}>
+      <Flex sx={{gridArea:'actions', gap:1}}>
         { !isCurrentUser ? <Button variant="blank" sx={{...buttonStyles,color:'moderateblue'}}>
         
           <svg sx={{mr:'6px'}} width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" /></svg>
@@ -83,6 +83,6 @@ const PresentCommentText : React.FunctionComponent<IPresentCommentText> = ({text
           </>
           
         }
-      </Box>
+      </Flex>
   </>
 }
