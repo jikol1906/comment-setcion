3a30f1b20cd24671c67c2cd42be9705163cbb9ed
Boris Grunwald <borisgrunwald@gmail.com>
2022-02-18 16:10:45 +0100

    created comment interface

---
+++ src/interfaces.ts
@@ -0,0 +1,27 @@
+export interface Comment {
+  id: number;
+  content: string;
+  createdAt: string;
+  score: number;
+  user: User;
+  replies?: Reply[];
+}
+
+export interface Reply {
+  id: number;
+  content: string;
+  createdAt: string;
+  score: number;
+  replyingTo: string;
+  user: User;
+}
+
+export interface User {
+  image: Image;
+  username: string;
+}
+
+export interface Image {
+  png: string;
+  webp: string;
+}
