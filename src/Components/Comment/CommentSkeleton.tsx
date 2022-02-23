cfb2f4e9f04ab0b100aeba421466e60802155ccd
Boris Grunwald <borisgrunwald@gmail.com>
2022-02-23 13:24:51 +0100

    basic commentskeleton

--- src/Components/Comment/CommentSkeleton.tsx
+++ src/Components/Comment/CommentSkeleton.tsx
@@ -1,22 +1,16 @@
 import CommentContainer from "../../Layout/CommentContainer/CommentContainer";
 import LikeDislikeButton from "../LikeDislikeButton/LikeDislikeButton";
 
-interface CommentSkeletonProps {
-  userInfo: React.ReactNode;
-  content: React.ReactNode;
-  buttons: React.ReactNode;
-  score: number;
-}
 //Component that is responsible for the layout of the comment
-const CommentSkeleton: React.FunctionComponent<CommentSkeletonProps> = ({
-  userInfo,
-  content,
-  buttons,
-  score,
+const CommentSkeleton: React.FunctionComponent = ({
+  children,
 }) => {
   return (
     <CommentContainer>
       <div className="grid gap-4 grid-cols-[auto__1fr__auto] md:gap-6">
+        {children}
+      </div>
+      {/* <div className="grid gap-4 grid-cols-[auto__1fr__auto] md:gap-6">
         <div className="col-span-full md:col-start-2 md:col-end-3">{userInfo}</div>
         <div className="col-span-full md:col-start-2 md:col-end-4">{content}</div>
         <div className="md:col-start-1 md:col-end-2 md:row-start-1 md:row-end-3">
@@ -25,7 +19,7 @@ const CommentSkeleton: React.FunctionComponent<CommentSkeletonProps> = ({
         <div className="flex col-start-3 space-x-2 md:row-start-1 md:row-end-2">
           {buttons}
         </div>
-      </div>
+      </div> */}
     </CommentContainer>
   );
 };
