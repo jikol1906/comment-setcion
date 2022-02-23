00eeab82da9941f10e6e869b4cfaead888c5a59b
Boris Grunwald <borisgrunwald@gmail.com>
2022-02-23 14:31:15 +0100

    wip

--- src/Components/Comment/CommentSkeleton.tsx
+++ src/Components/Comment/CommentSkeleton.tsx
@@ -10,16 +10,6 @@ const CommentSkeleton: React.FunctionComponent = ({
       <div className="grid gap-4 grid-cols-[auto__1fr__auto] md:gap-6">
         {children}
       </div>
-      {/* <div className="grid gap-4 grid-cols-[auto__1fr__auto] md:gap-6">
-        <div className="col-span-full md:col-start-2 md:col-end-3">{userInfo}</div>
-        <div className="col-span-full md:col-start-2 md:col-end-4">{content}</div>
-        <div className="md:col-start-1 md:col-end-2 md:row-start-1 md:row-end-3">
-          <LikeDislikeButton score={score} />
-        </div>
-        <div className="flex col-start-3 space-x-2 md:row-start-1 md:row-end-2">
-          {buttons}
-        </div>
-      </div> */}
     </CommentContainer>
   );
 };
--- src/Components/Comment/CurrentUserComment.tsx
+++ src/Components/Comment/CurrentUserComment.tsx
@@ -37,34 +37,49 @@ const CurrentUserComment: React.FunctionComponent<ICurrentUserComment> = ({
         <UserInfo user={user} />
       </div>
       <div className="col-span-full md:col-start-2 md:col-end-4">
-        {textSection}
+        {editing ? (
+          <>
+            <form id="updateform">
+              <TextArea rows={4} value={content}></TextArea>
+            </form>
+          </>
+        ) : (
+          <CommentText text={content} />
+        )}
       </div>
       <div className="md:col-start-1 md:col-end-2 md:row-start-1 md:row-end-3">
-          <LikeDislikeButton score={score} />
+        <LikeDislikeButton score={score} />
       </div>
-      <div className="flex col-start-3 space-x-2 md:row-start-1 md:row-end-2">
-        <IconButton
-          variant="danger"
-          onClick={onDeleteButtonClicked}
-          icon={
-            <svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
-              <path d="M2.167 12.448C2.167 13.302 2.867 14 3.722 14H9.944C10.8 14 11.5 13.302 11.5 12.448V3.5H2.167V12.448ZM12.667 1.167H9.75L8.773 0H4.893L3.917 1.167H1V2.333H12.667V1.167V1.167Z" />
-            </svg>
-          }
-        >
-          Delete
-        </IconButton>
-        <IconButton
-          variant="normal"
-          icon={
-            <svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
-              <path d="M13.479 2.87195L11.08 0.473947C10.767 0.180345 10.357 0.0120105 9.92792 0.000948149C9.49889 -0.0101142 9.08075 0.136867 8.753 0.413947L0.879 8.28695C0.595453 8.57231 0.418879 8.94665 0.379 9.34695L0.00399967 12.9949C-0.00700433 13.1163 0.00746647 13.2386 0.0464849 13.3541C0.0855033 13.4696 0.148209 13.5756 0.230584 13.6654C0.312959 13.7552 0.413187 13.8268 0.524846 13.8757C0.636505 13.9245 0.757132 13.9494 0.879 13.9489H0.957L4.607 13.6159C5.006 13.5759 5.38 13.3999 5.665 13.1169L13.54 5.24195C13.8456 4.91909 14.0108 4.48826 13.9994 4.04384C13.9879 3.59943 13.8008 3.17766 13.479 2.87095V2.87195ZM10.504 5.79495L8.159 3.44895L9.865 1.69995L12.254 4.08995L10.504 5.79595V5.79495Z" />
-            </svg>
-          }
-        >
-          Edit
-        </IconButton>
-      </div>
+      {!editing && (
+        <div className="flex col-start-3 space-x-2 md:row-start-1 md:row-end-2">
+          <IconButton
+            variant="danger"
+            onClick={onDeleteButtonClicked}
+            icon={
+              <svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+                <path d="M2.167 12.448C2.167 13.302 2.867 14 3.722 14H9.944C10.8 14 11.5 13.302 11.5 12.448V3.5H2.167V12.448ZM12.667 1.167H9.75L8.773 0H4.893L3.917 1.167H1V2.333H12.667V1.167V1.167Z" />
+              </svg>
+            }
+          >
+            Delete
+          </IconButton>
+          <IconButton
+            variant="normal"
+            icon={
+              <svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13.479 2.87195L11.08 0.473947C10.767 0.180345 10.357 0.0120105 9.92792 0.000948149C9.49889 -0.0101142 9.08075 0.136867 8.753 0.413947L0.879 8.28695C0.595453 8.57231 0.418879 8.94665 0.379 9.34695L0.00399967 12.9949C-0.00700433 13.1163 0.00746647 13.2386 0.0464849 13.3541C0.0855033 13.4696 0.148209 13.5756 0.230584 13.6654C0.312959 13.7552 0.413187 13.8268 0.524846 13.8757C0.636505 13.9245 0.757132 13.9494 0.879 13.9489H0.957L4.607 13.6159C5.006 13.5759 5.38 13.3999 5.665 13.1169L13.54 5.24195C13.8456 4.91909 14.0108 4.48826 13.9994 4.04384C13.9879 3.59943 13.8008 3.17766 13.479 2.87095V2.87195ZM10.504 5.79495L8.159 3.44895L9.865 1.69995L12.254 4.08995L10.504 5.79595V5.79495Z" />
+              </svg>
+            }
+          >
+            Edit
+          </IconButton>
+        </div>
+      )}
+      {editing && (
+        <button type="submit" form="updateform" className="btn col-start-3">
+          Update
+        </button>
+      )}
     </CommentSkeleton>
   );
 };
