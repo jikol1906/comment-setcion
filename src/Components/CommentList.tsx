8e8729c9e8b022b94a413c1a4574d68a7195d2d0
Boris Grunwald <borisgrunwald@gmail.com>
2022-02-23 15:13:34 +0100

    refactor commentlist

--- src/Components/CommentList.tsx
+++ src/Components/CommentList.tsx
@@ -4,15 +4,15 @@ import AddComment from "./AddComment/AddComment";
 import Comment from "../Components/Comment/Comment";
 import CurrentUserComment from "./Comment/CurrentUserComment";
 import data from "../data.json";
-import { Comment as IComment, ReplyInfo } from '../interfaces'
+import { Comment as IComment, Reply, ReplyInfo } from "../interfaces";
 const currentUser = data.currentUser;
 
 interface ICommentListProps {
   comments: IComment[];
   replyingTo: number;
-  deleteComment: (commentId:number) => void;
-  setReplyingTo: (commentId:number) => void;
-  reply:(commentId: number, replyInfo: ReplyInfo) => void
+  deleteComment: (commentId: number) => void;
+  setReplyingTo: (commentId: number) => void;
+  reply: (commentId: number, replyInfo: ReplyInfo) => void;
 }
 
 const CommentList: React.FunctionComponent<ICommentListProps> = ({
@@ -20,31 +20,41 @@ const CommentList: React.FunctionComponent<ICommentListProps> = ({
   replyingTo,
   deleteComment,
   setReplyingTo,
-  reply
+  reply,
 }) => {
   let commentsRendered: JSX.Element[] = [];
 
-  comments.forEach((c) => {
-    commentsRendered.push(
-      <React.Fragment key={c.id}>
-        {c.user.username === currentUser.username ? (
-          <CurrentUserComment
-            {...c}
-            onDeleteButtonClicked={() => deleteComment(c.id)}
-          />
-        ) : (
-          <Comment onReplyButtonClicked={() => setReplyingTo(c.id)} {...c} />
-        )}
-        {replyingTo === c.id && (
-          <AddComment
-            submit={() => reply(c.id, {
+  const createComment = (c: IComment | Reply) => {
+    const comment =
+      c.user.username === currentUser.username ? (
+        <CurrentUserComment
+          {...c}
+          onDeleteButtonClicked={() => deleteComment(c.id)}
+        />
+      ) : (
+        <Comment onReplyButtonClicked={() => setReplyingTo(c.id)} {...c} />
+      );
+
+    return replyingTo === c.id ? (
+      <>
+        <AddComment
+          submit={() =>
+            reply(c.id, {
               replyingToUsername: c.user.username,
               topLevelCommentId: c.id,
-            })}
-            replying
-          />
-        )}
-      </React.Fragment>
+            })
+          }
+          replying
+        />
+      </>
+    ) : (
+      comment
+    );
+  };
+
+  comments.forEach((c) => {
+    commentsRendered.push(
+      <React.Fragment key={c.id}>{createComment(c)}</React.Fragment>
     );
 
     if (c.replies.length > 0) {
@@ -53,26 +63,7 @@ const CommentList: React.FunctionComponent<ICommentListProps> = ({
           {c.replies.map((r) => {
             return (
               <React.Fragment key={r.id}>
-                {r.user.username === currentUser.username ? (
-                  <CurrentUserComment
-                    {...r}
-                    onDeleteButtonClicked={() => deleteComment(r.id)}
-                  />
-                ) : (
-                  <Comment
-                    onReplyButtonClicked={() => setReplyingTo(r.id)}
-                    {...r}
-                  />
-                )}
-                {replyingTo === r.id && (
-                  <AddComment
-                    submit={() => reply(r.id, {
-                      replyingToUsername: r.user.username,
-                      topLevelCommentId: c.id,
-                    })}
-                    replying
-                  />
-                )}
+                  {createComment(r)}
               </React.Fragment>
             );
           })}
