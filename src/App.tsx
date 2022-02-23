cc6dbea07a4f0b69b0a054902a4e578a47c4a6e3
Boris Grunwald <borisgrunwald@gmail.com>
2022-02-23 14:54:22 +0100

    implement commentlist

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import useComments from "./useComment";
 import data from "./data.json";
 import React from "react";
 import CurrentUserComment from "./Components/Comment/CurrentUserComment";
+import CommentList from "./Components/CommentList";
 
 const currentUser = data.currentUser;
 
@@ -12,57 +13,15 @@ const currentUser = data.currentUser;
 function App() {
   const { comments, replyingTo, setReplyingTo,reply, addComment,deleteComment } = useComments();
 
-  let commentsRendered: JSX.Element[] = [];
-
-  comments.forEach((c) => {
-    
-    commentsRendered.push(
-      <React.Fragment key={c.id}>
-        {c.user.username === currentUser.username ?
-          <CurrentUserComment {...c} onDeleteButtonClicked={() => deleteComment(c.id)}/>:
-          <Comment onReplyButtonClicked={() => setReplyingTo(c.id)} {...c} />
-        }
-        {replyingTo === c.id && <AddComment submit={reply(c.id,{replyingToUsername:c.user.username, topLevelCommentId:c.id})} replying />}
-      </React.Fragment>
-    );
-
-    if (c.replies.length > 0) {
-      commentsRendered.push(
-        <CommentReplyThread key={`r${c.id}`}>
-          {c.replies.map((r) => {
-            return (
-              <React.Fragment key={r.id}>
-                {r.user.username === currentUser.username ? (
-                  <CurrentUserComment
-                    {...r}
-                    onDeleteButtonClicked={() => deleteComment(r.id)}
-                  />
-                ) : (
-                  <Comment
-                    onReplyButtonClicked={() => setReplyingTo(r.id)}
-                    {...r}
-                  />
-                )}
-                {replyingTo === r.id && (
-                  <AddComment
-                    submit={reply(r.id, {
-                      replyingToUsername: r.user.username,
-                      topLevelCommentId: c.id,
-                    })}
-                    replying
-                  />
-                )}
-              </React.Fragment>
-            );
-          })}
-        </CommentReplyThread>
-      );
-    }
-  });
-
   return (
     <div className="grid gap-2 p-2 max-w-4xl mx-auto py-20">
-      {commentsRendered}
+      <CommentList
+        comments={comments}
+        deleteComment={deleteComment}
+        reply={reply}
+        setReplyingTo={setReplyingTo}
+        replyingTo={replyingTo}
+      />
       <AddComment submit={addComment} />
     </div>
   );
