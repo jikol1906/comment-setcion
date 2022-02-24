9c7e0484b32c378cef9eb79bf48dc3ea44965342
Boris Grunwald <borisgrunwald@gmail.com>
2022-02-24 18:48:14 +0100

    wrapped addcomment in commentskeleton

--- src/Components/AddComment/AddComment.tsx
+++ src/Components/AddComment/AddComment.tsx
@@ -3,6 +3,7 @@ import CommentContainer from "../../Layout/CommentContainer/CommentContainer";
 import data from "../../data.json";
 import { useState } from "react";
 import TextArea from "../TextArea/TextArea";
+import CommentSkeleton from "../Comment/CommentSkeleton";
 
 const currentUser = data.currentUser;
 
@@ -21,36 +22,34 @@ const AddComment: React.FunctionComponent<{
   };
 
   return (
-    <CommentContainer>
-      <div className="grid gap-2 grid-cols-[auto__1fr__auto]">
-        <form
-          className="col-span-full"
-          id={replying ? "replycommentform":"sendcommentform"}
-          onSubmit={(e) => submitHandler(e)}
-        >
-          <TextArea
-            required
-            placeholder="Add a comment..."
-            rows={3}
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          ></TextArea>
-        </form>
-        <img
-          className="w-9"
-          src={`${process.env.PUBLIC_URL}${
-            currentUser.image.webp.split("./")[1]
-          }`}
-        />
-        <button
-          type="submit"
-          className="btn col-start-3 self-center btn-hover-styles"
-          form={replying ? "replycommentform":"sendcommentform"}
-        >
-            {replying ? "Reply" : "Send"}
-        </button>
-      </div>
-    </CommentContainer>
+    <CommentSkeleton>
+      <form
+        className="col-span-full"
+        id={replying ? "replycommentform":"sendcommentform"}
+        onSubmit={(e) => submitHandler(e)}
+      >
+        <TextArea
+          required
+          placeholder="Add a comment..."
+          rows={3}
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></TextArea>
+      </form>
+      <img
+        className="w-9"
+        src={`${process.env.PUBLIC_URL}${
+          currentUser.image.webp.split("./")[1]
+        }`}
+      />
+      <button
+        type="submit"
+        className="btn col-start-3 self-center btn-hover-styles"
+        form={replying ? "replycommentform":"sendcommentform"}
+      >
+          {replying ? "Reply" : "Send"}
+      </button>
+    </CommentSkeleton>
   );
 };
 
