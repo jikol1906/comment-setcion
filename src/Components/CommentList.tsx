947503cbbf5eee3ef4ad0e7c8594e4a116b8fbe5
Boris Grunwald <borisgrunwald@gmail.com>
2022-02-27 19:20:13 +0100

    removed unecessart

--- src/Components/CommentList.tsx
+++ src/Components/CommentList.tsx
@@ -28,65 +28,6 @@ const CommentList: React.FunctionComponent = () => {
 
   }
   
-
-  // let commentsRendered: JSX.Element[] = []; 
-    
-  //   const createComment = (c: IComment) => {
-  //     const comment =
-  //       c.user.username === currentUser.username ? (
-  //         <CurrentUserComment
-  //           {...c}
-  //           onDeleteButtonClicked={() => deleteComment(c.id)}
-  //         />
-  //       ) : (
-  //         <Comment onReplyButtonClicked={() => setReplyingTo(c.id)} {...c} />
-  //       );
-
-  //       if(c.hasReplies) {
-  //         console.log('here');
-          
-  //          getReplies(c.id)?.then(r => {
-  //            r.forEach(r => console.log(r.data()))
-  //          })
-            
-          
-          
-           
-  //       }
-  
-  //     return comment
-
-  //   };
-  
-  //   comments?.forEach((c) => {
-      
-      
-      
-      
-  //     commentsRendered.push(
-  //       <React.Fragment key={c.id}>{createComment(c)}</React.Fragment>
-  //     );
-  //   })
-  
-  
-  
-
-  
-  //   if (c.replies.length > 0) {
-  //     commentsRendered.push(
-  //       <CommentReplyThread key={`r${c.id}`}>
-  //         {c.replies.map((r) => {
-  //           return (
-  //             <React.Fragment key={r.id}>
-  //                 {createComment(r)}
-  //             </React.Fragment>
-  //           );
-  //         })}
-  //       </CommentReplyThread>
-  //     );
-  //   }
-  // });
-
  
   return <>
     {value && value.map(v => {
