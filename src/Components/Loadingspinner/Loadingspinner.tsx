f263fa6f35dab2f5469476a42099240c0d80d55a
Boris Grunwald <borisgrunwald@gmail.com>
2022-03-02 12:29:50 +0100

    created loadingspinner

---
+++ src/Components/Loadingspinner/Loadingspinner.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import CommentSkeleton from '../Comment/CommentSkeleton';
+import './Loadingspinner.css'
+
+interface ILoadingspinnerProps {
+}
+
+const Loadingspinner: React.FunctionComponent<ILoadingspinnerProps> = (props) => {
+  return <CommentSkeleton>
+      <div className="flex justify-center col-span-full text-moderateblue">
+          <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+      </div>
+  </CommentSkeleton>;
+};
+
+export default Loadingspinner;
