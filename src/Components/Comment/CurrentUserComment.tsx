176298c60e3371ff9a7ce42c358aa06554d536d2
Boris Grunwald <borisgrunwald@gmail.com>
2022-03-02 12:52:12 +0100

    make currentusercomment take care of editing state

--- src/Components/Comment/CurrentUserComment.tsx
+++ src/Components/Comment/CurrentUserComment.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { CommentProps } from "../../interfaces";
 import IconButton from "../Button/IconButton";
 import LikeDislikeButton from "../LikeDislikeButton/LikeDislikeButton";
@@ -8,7 +9,6 @@ import CommentText from "./CommentText";
 
 interface ICurrentUserComment extends CommentProps {
   onDeleteButtonClicked: () => void;
-  editing?: boolean;
 }
 
 const CurrentUserComment: React.FunctionComponent<ICurrentUserComment> = ({
@@ -16,8 +16,10 @@ const CurrentUserComment: React.FunctionComponent<ICurrentUserComment> = ({
   content,
   score,
   user,
-  editing,
 }) => {
+
+  const [editing,setEditing] = useState(false)
+
   return (
     <CommentSkeleton>
       <div className="col-span-full md:col-start-2 md:col-end-3">
@@ -52,6 +54,7 @@ const CurrentUserComment: React.FunctionComponent<ICurrentUserComment> = ({
           </IconButton>
           <IconButton
             variant="normal"
+            onClick={_ => setEditing(true)}
             icon={
               <svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
                 <path d="M13.479 2.87195L11.08 0.473947C10.767 0.180345 10.357 0.0120105 9.92792 0.000948149C9.49889 -0.0101142 9.08075 0.136867 8.753 0.413947L0.879 8.28695C0.595453 8.57231 0.418879 8.94665 0.379 9.34695L0.00399967 12.9949C-0.00700433 13.1163 0.00746647 13.2386 0.0464849 13.3541C0.0855033 13.4696 0.148209 13.5756 0.230584 13.6654C0.312959 13.7552 0.413187 13.8268 0.524846 13.8757C0.636505 13.9245 0.757132 13.9494 0.879 13.9489H0.957L4.607 13.6159C5.006 13.5759 5.38 13.3999 5.665 13.1169L13.54 5.24195C13.8456 4.91909 14.0108 4.48826 13.9994 4.04384C13.9879 3.59943 13.8008 3.17766 13.479 2.87095V2.87195ZM10.504 5.79495L8.159 3.44895L9.865 1.69995L12.254 4.08995L10.504 5.79595V5.79495Z" />
