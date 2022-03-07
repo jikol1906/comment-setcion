d2e31056efb0d01057220ddae26c1915583a645d
Boris Grunwald <borisgrunwald@gmail.com>
2022-03-07 11:14:50 +0100

    format

--- src/Firebase.test.ts
+++ src/Firebase.test.ts
@@ -1,178 +1,186 @@
 /**
  * @jest-environment node
  */
-import fs from 'fs'
+import fs from "fs";
 
 // Assuming a Firestore app and the Firestore emulator for this example
-import { addDoc, collection, deleteDoc, doc, FieldValue, Firestore, getDoc, getDocs, increment, query, serverTimestamp, setDoc, Timestamp, updateDoc, writeBatch } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  FieldValue,
+  Firestore,
+  getDoc,
+  getDocs,
+  increment,
+  query,
+  serverTimestamp,
+  setDoc,
+  Timestamp,
+  updateDoc,
+  writeBatch,
+} from "firebase/firestore";
 
 import {
-    assertFails,
-    assertSucceeds,
-    initializeTestEnvironment,
-    RulesTestEnvironment,
-} from "@firebase/rules-unit-testing"
-import { Comment, User } from './interfaces';
+  assertFails,
+  assertSucceeds,
+  initializeTestEnvironment,
+  RulesTestEnvironment,
+} from "@firebase/rules-unit-testing";
+import { Comment, User } from "./interfaces";
 
-const newComment : Partial<Comment> = {
-  createdAt:serverTimestamp(),
-  content:"sef",
-  score:0,
-  parentComment:null,
-  hasReplies:false
-}
+const newComment: Partial<Comment> = {
+  createdAt: serverTimestamp(),
+  content: "sef",
+  score: 0,
+  parentComment: null,
+  hasReplies: false,
+};
 
-    
-
-let testEnv : RulesTestEnvironment;
+let testEnv: RulesTestEnvironment;
 
 beforeAll(async () => {
-
-testEnv = await initializeTestEnvironment({
+  testEnv = await initializeTestEnvironment({
     projectId: "comment-section-e9c09",
     firestore: {
-        rules: fs.readFileSync('firestore.rules', 'utf8'),
-        host:'localhost',
-        port:1234
+      rules: fs.readFileSync("firestore.rules", "utf8"),
+      host: "localhost",
+      port: 1234,
     },
-});
-
+  });
 });
 
 beforeEach(async () => {
-    await testEnv.clearFirestore();
-})
+  await testEnv.clearFirestore();
+});
 
 afterAll(async () => {
-    await testEnv.cleanup();
-    
-
-})
+  await testEnv.cleanup();
+});
 
 test("users can only access their own documents", async () => {
-    
-        const juliusomo_id = "test_user"
-        const juliusomo = testEnv.authenticatedContext(juliusomo_id);
-        const juliusomo_db = juliusomo.firestore();
-
-        const bob_id = "test_user2"
-        const bob = testEnv.authenticatedContext(bob_id);
-        const bob_db = bob.firestore()
-
-        const theDoc = doc(juliusomo_db,juliusomo_id,"test")
-
-        await assertSucceeds(setDoc(theDoc,newComment))
-
-        await assertFails(getDoc(doc(bob_db,juliusomo_id,"test")))
-        await assertSucceeds(getDoc(doc(juliusomo_db,juliusomo_id,"test")))
-
-        
-        
-
-        
-})
-
-test("users can only increment counter by one", async () => {
-  const juliusomo_id = "test_user"
+  const juliusomo_id = "test_user";
   const juliusomo = testEnv.authenticatedContext(juliusomo_id);
   const juliusomo_db = juliusomo.firestore();
 
-  const theDoc = doc(juliusomo_db,juliusomo_id,"test")
+  const bob_id = "test_user2";
+  const bob = testEnv.authenticatedContext(bob_id);
+  const bob_db = bob.firestore();
 
-  await assertSucceeds(setDoc(theDoc,newComment))
+  const theDoc = doc(juliusomo_db, juliusomo_id, "test");
 
-  await assertSucceeds(updateDoc(theDoc,{
-    score:increment(1)
-  }))
-  await assertSucceeds(updateDoc(theDoc,{
-    score:increment(1)
-  }))
+  await assertSucceeds(setDoc(theDoc, newComment));
 
-  await assertFails(updateDoc(theDoc,{
-    score:increment(2)
-  }))
+  await assertFails(getDoc(doc(bob_db, juliusomo_id, "test")));
+  await assertSucceeds(getDoc(doc(juliusomo_db, juliusomo_id, "test")));
+});
 
-  await assertSucceeds(updateDoc(theDoc,{
-    score:increment(1)
-  }))
+test("users can only increment counter by one", async () => {
+  const juliusomo_id = "test_user";
+  const juliusomo = testEnv.authenticatedContext(juliusomo_id);
+  const juliusomo_db = juliusomo.firestore();
 
-})
+  const theDoc = doc(juliusomo_db, juliusomo_id, "test");
 
+  await assertSucceeds(setDoc(theDoc, newComment));
 
+  await assertSucceeds(
+    updateDoc(theDoc, {
+      score: increment(1),
+    })
+  );
+  await assertSucceeds(
+    updateDoc(theDoc, {
+      score: increment(1),
+    })
+  );
 
+  await assertFails(
+    updateDoc(theDoc, {
+      score: increment(2),
+    })
+  );
 
-const seedDatabase = async (uid: string,db:any) => {
-    const commentsColl = collection(db, uid);
-  
-    const batch = writeBatch(db);
-  
-    const user : User ={
-          image: {
-            png: "./images/avatars/image-juliusomo.png",
-            webp: "./images/avatars/image-juliusomo.webp",
-          },
-          username: "juliusomo",
-          userId:uid
-    }
-  
-    const q = query(commentsColl);
-  
-    const snap = await getDocs(q);
-  
-    if (snap.empty) {
-      batch.set(doc(commentsColl), {
-        content:
-          "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-        createdAt: Timestamp.now(),
-        score: 12,
-        user,
-        parentComment: null,
-        hasReplies: false,
-      });
-  
-      const docWithReplies = doc(commentsColl);
-  
-      batch.set(docWithReplies, {
-        content: "A comment that has some replies",
-        createdAt: "1 month ago",
-        score: 12,
-        user: {
-          image: {
-            png: "./images/avatars/image-amyrobson.png",
-            webp: "./images/avatars/image-amyrobson.webp",
-          },
-          username: "amyrobson",
-          userId:"123"
-        },
-        parentComment: null,
-        hasReplies: true,
-      });
-  
-      batch.set(doc(commentsColl), {
-        content: "A comment that has some replies",
-        createdAt: "1 month ago",
-        score: 12,
-        user: {
-          image: {
-            png: "./images/avatars/image-amyrobson.png",
-            webp: "./images/avatars/image-amyrobson.webp",
-          },
-          username: "amyrobson",
-          userId:"234"
-        },
-        parentComment: docWithReplies.id,
-        hasReplies: false,
-      });
-  
-      batch.set(doc(commentsColl), {
-        content: "I agree lol",
-        createdAt: "1 month ago",
-        score: 12,
-        user,
-        parentComment: docWithReplies.id,
-        hasReplies: false,
-      });
-    }
-  
-    await batch.commit();
+  await assertSucceeds(
+    updateDoc(theDoc, {
+      score: increment(1),
+    })
+  );
+});
+
+const seedDatabase = async (uid: string, db: any) => {
+  const commentsColl = collection(db, uid);
+
+  const batch = writeBatch(db);
+
+  const user: User = {
+    image: {
+      png: "./images/avatars/image-juliusomo.png",
+      webp: "./images/avatars/image-juliusomo.webp",
+    },
+    username: "juliusomo",
+    userId: uid,
   };
+
+  const q = query(commentsColl);
+
+  const snap = await getDocs(q);
+
+  if (snap.empty) {
+    batch.set(doc(commentsColl), {
+      content:
+        "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+      createdAt: Timestamp.now(),
+      score: 12,
+      user,
+      parentComment: null,
+      hasReplies: false,
+    });
+
+    const docWithReplies = doc(commentsColl);
+
+    batch.set(docWithReplies, {
+      content: "A comment that has some replies",
+      createdAt: "1 month ago",
+      score: 12,
+      user: {
+        image: {
+          png: "./images/avatars/image-amyrobson.png",
+          webp: "./images/avatars/image-amyrobson.webp",
+        },
+        username: "amyrobson",
+        userId: "123",
+      },
+      parentComment: null,
+      hasReplies: true,
+    });
+
+    batch.set(doc(commentsColl), {
+      content: "A comment that has some replies",
+      createdAt: "1 month ago",
+      score: 12,
+      user: {
+        image: {
+          png: "./images/avatars/image-amyrobson.png",
+          webp: "./images/avatars/image-amyrobson.webp",
+        },
+        username: "amyrobson",
+        userId: "234",
+      },
+      parentComment: docWithReplies.id,
+      hasReplies: false,
+    });
+
+    batch.set(doc(commentsColl), {
+      content: "I agree lol",
+      createdAt: "1 month ago",
+      score: 12,
+      user,
+      parentComment: docWithReplies.id,
+      hasReplies: false,
+    });
+  }
+
+  await batch.commit();
+};
