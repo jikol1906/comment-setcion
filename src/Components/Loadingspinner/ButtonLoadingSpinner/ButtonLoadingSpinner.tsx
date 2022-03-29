4a9fcb5acefafea2bd0a54650e534a3fe7580932
Boris Grunwald <borisgrunwald@gmail.com>
2022-03-29 23:04:58 +0200

    added white to buttonloadingspinner

--- src/Components/Loadingspinner/ButtonLoadingSpinner/ButtonLoadingSpinner.tsx
+++ src/Components/Loadingspinner/ButtonLoadingSpinner/ButtonLoadingSpinner.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import './ButtonLoadingSpinner.css'
 
 interface IButtonLoadingSpinnerProps {
+  white?:boolean
 }
 
-const ButtonLoadingSpinner: React.FunctionComponent<IButtonLoadingSpinnerProps> = (props) => {
-  return <div className="loader"></div>;
+const ButtonLoadingSpinner: React.FunctionComponent<IButtonLoadingSpinnerProps> = ({white}) => {
+  return <div className={`loader m-auto ${white && 'white'}`}></div>;
 };
 
 export default ButtonLoadingSpinner;
