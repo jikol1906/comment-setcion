dbeb4a0209c32065f45e704412881dba43be4529
Boris Grunwald <borisgrunwald@gmail.com>
2022-03-29 12:01:40 +0200

    store button width in ref

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
+import { useLayoutEffect, useRef } from 'react';
 
 interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     loading?:boolean
 }
 
 const Button: React.FunctionComponent<IButtonProps> = ({loading,children,...rest}) => {
+
+    const btnRef = useRef<HTMLButtonElement>(null)
+    const btnWidth = useRef<number|null>(null)
+
+    useLayoutEffect(() => {
+        if(btnRef.current) {
+            btnWidth.current = btnRef.current.getBoundingClientRect().width
+        }
+    })
+
   return <button
   className="btn btn-hover-styles leading-none"
+  ref={btnRef}
    {...rest}>
       {children}
   </button>;
